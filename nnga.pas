a156c1f52d19b9ac9dd3d53318d5dc620cdb83ac
goFrendiAsgard <goFrendiAsgard@gmail.com>
2011-11-19 13:39:02 +0700

    edited by goFrendiAsgard, use class approach for neural network, database completed

--- nnga.pas
+++ nnga.pas
@@ -4,27 +4,76 @@ unit Unit2;
 
 interface
 
-type
-    //TODO : consider to use class
-    TNeuralNetwork = record
-      wights      : array[0..2] of array of real; //0: input, 1: hidden, 2: output
-      inputLayer  : integer; //count of input layer
-      hiddenLayer : integer; //count of hidden layer
-      outputLayer : integer; //count of output layer
-    end;
-
-//generate random weights as parameter given
-function makeNeuralNetwork(inputLayer, hiddenLayer, outputLayer) : TNeuralNetwork;
-//forward to generate output
-function forward(neuralNetwork : TNeuralNetwork; input: array of real) : array of real;
-//the backprop process, use forward to generate output, find delta by compare it with desiredOutput
-//do calculation to adjust weight of neuralNetwork
-procedure backprop(var neuralNetwork : TNeuralNetwork; desiredOutput : array of real);
-
 uses
   Classes, SysUtils, Math;
 
+type
+    //TODO : consider to use class
+    TNeuralNetwork = class
+    private
+      weights      : array of array of real; //0: input-hidden1, 1: hidden1-hidden2, n: output
+      inputNeuron  : integer; //count of input neuron
+      hiddenNeuron : array of integer; //count of hidden neuron
+      outputNeuron : integer; //count of output neuron
+
+    public
+      //generate random weights as parameter given
+      procedure makeNeuralNetwork(inputNeuron : integer; hiddenNeuron : array of integer; outputNeuron:integer);
+      //forward to generate output
+      procedure calculate_forward(neuralNetwork : TNeuralNetwork; input: array of real; var delta : array of real);
+      //the backprop process, use forward to generate output, find delta by compare it with desiredOutput
+      //do calculation to adjust weight of neuralNetwork
+      procedure calculate_backprop(var neuralNetwork : TNeuralNetwork; desiredOutput : array of real);
+    end;
+
+
 implementation
+
+procedure TNeuralNetwork.makeNeuralNetwork(inputNeuron : integer; hiddenNeuron : array of integer; outputNeuron:integer);
+var
+   i, j : integer;
+   neuronCount : array of integer;
+begin
+   //NOTE FOR JAVA PROGRAMMER : in java we use 'This', in lazarus or delphi we use 'self' instead
+   self.inputNeuron:=inputNeuron;
+   self.outputNeuron:=outputNeuron;
+   self.hiddenNeuron:=hiddenNeuron;
+   setlength(self.weights, 1+length(self.hiddenNeuron));
+
+   //neuron count for each layer
+   setlength(neuronCount, 2+length(self.hiddenNeuron));
+   for i := 0 to length(neuronCount-1) do
+   begin
+     //Tentukan panjang dari masing-masing weights
+     if i = 0 then
+        neuronCount[i]:=inputNeuron
+     else if i = length(self.weights)-1 do
+        neuronCount[i]:=outputNeuron
+     else
+        neuronCount[i]:=hiddenNeuron[i+1];
+   end;
+
+   //weights between each layer
+   for i := 0 to length(self.weights) do
+   begin
+     setlength(self.weights[i],neuronCount[i]*neuronCount[i+1]);
+     for j := 0 to length(self.weights[i]) do
+     begin
+       Math.Randomize;
+       self.weights[i][j]:=Math.Random;
+     end;
+   end;
+end;
+
+procedure TNeuralNetwork.calculate_forward(neuralNetwork : TNeuralNetwork; input: array of real; var delta : array of real);
+begin
+
+end;
+
+procedure TNeuralNetwork.calculate_backprop(var neuralNetwork : TNeuralNetwork; desiredOutput : array of real);
+begin
+
+end;
 
 end.
 
